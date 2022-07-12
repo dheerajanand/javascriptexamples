15e236981acecaaf75117665568d28bd1a5fdab6
Dheeraj Anand <dheeru.anand22@gmail.com>
2022-07-12 10:44:56 +0530

    Added Code

--- dataGrouping.js
+++ dataGrouping.js
@@ -50,21 +50,3 @@ function groupByMethod(objectArray, property) {
 }
 console.log(groupByMethod(data, "class"));
 // https://www.tutorialspoint.com/most-efficient-method-to-groupby-on-an-array-of-objects-in-javascript
-const people = [
-  { name: "Lee", age: 21 },
-  { name: "Ajay", age: 20 },
-  { name: "Jane", age: 20 },
-];
-function groupBy(objectArray, property) {
-  return objectArray.reduce((acc, obj) => {
-    const key = obj[property];
-    if (!acc[key]) {
-      acc[key] = [];
-    }
-    // Add object to list for given key's value
-    acc[key].push(obj);
-    return acc;
-  }, {});
-}
-console.log(groupBy(people, "age"));
-console.log(groupBy(people, "name"));
