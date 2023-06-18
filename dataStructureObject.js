410aa259efadd7ca9eecc131c7d5ebc45d7ee5e1
Dheeraj Anand <dheerajanand@Dheerajs-MacBook-Pro.local>
2023-06-18 16:46:13 +0530

    Data Structure Array Reducer Added

--- dataStructureObject.js
+++ dataStructureObject.js
@@ -1,16 +1,25 @@
 const portfolio = [
-  { name: "Mark", stock: "FB" },
-  { name: "Steve", stock: "AAPL" },
-  { name: "Tim", stock: "AAPL" },
-  { name: "Steve", stock: "MSFT" },
-  { name: "Bill", stock: "MSFT" },
-  { name: "Bill", stock: "AAPL" },
+  { name: 'Mark', stock: 'FB' },
+  { name: 'Steve', stock: 'AAPL' },
+  { name: 'Tim', stock: 'AAPL' },
+  { name: 'Steve', stock: 'MSFT' },
+  { name: 'Bill', stock: 'MSFT' },
+  { name: 'Bill', stock: 'AAPL' },
 ];
 
 const shareholder = [
-  { stock: "AAPL", name: ["Steve", "Bill", "Tim"], count: 3 },
-  { stock: "MSFT", name: ["Steve", "Bill"], count: 2 },
-  { stock: "FB", name: ["Mark", "Bill", "Tim"], count: 1 },
+  { stock: 'AAPL', name: ['Steve', 'Bill', 'Tim'], count: 3 },
+  { stock: 'MSFT', name: ['Steve', 'Bill'], count: 2 },
+  { stock: 'FB', name: ['Mark', 'Bill', 'Tim'], count: 1 },
 ];
 
-//pending
+result = portfolio.reduce(function (r, a) {
+  key = a.name || 'others';
+  console.log(a);
+  console.log(key);
+  r[key] = r[key] || [];
+  r[key].push(a);
+  return r;
+}, Object.create(null));
+
+console.log('getShareholder ', result);
